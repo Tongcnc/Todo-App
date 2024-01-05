eae20916709b244ff72ed97161b7876c79342ef0
Tongcnc <tongcnc3@gmail.com>
2024-01-05 14:07:45 +0700

    set up project

---
+++ client/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      "dark-pink": "#E07C7C",
+      "light-pink": "#EBB9B8",
+      "background-color": "#E5E5E5",
+      white: "#FFFFFF",
+      black: "#000000",
+      dark: "#3B3B3B",
+      violet: "#585292",
+      "dark-gray": "#9796A8",
+      "light-gray": "#BCBCBC",
+      "medium-gray": "#A9A9A9",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
